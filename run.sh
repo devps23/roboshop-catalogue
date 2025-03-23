8a06ed144c6865c4370471cf2c6543b8153b5634
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-23 11:57:50 +0530

    debug schema

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 source data/secrets.txt
-
+echo data/secrets.txt
 cat data/secrets.txt
 echo
 echo
@@ -7,12 +7,12 @@ echo
 env
 # ,,,the above four steps are debugging whether these variables are available in env or not
 
-if [ -z "$DOCUMENTDB" ] ; then
+if [ -z "${DOCUMENTDB}" ] ; then
   echo Environment DOCUMENTDB is missing
   exit 1
 fi
 
-if [ -z "$MONGO_URL" ] ; then
+if [ -z "${MONGO_URL}" ] ; then
   echo Environment MONGO_URL is missing
   exit 1
 fi
